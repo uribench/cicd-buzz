182a7ec8ac9b1e6a96355d5c49867673988418e2
uribench <uribench@gmail.com>
2018-02-21 10:42:17 +0200

    Modify 'test_generator.py' to better use 'unittest' framework

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,20 +1,33 @@
-import unittest
+# test_generator.py
+#
+# This is not the original example code from https://medium.com/bettercode/how-to-build-a-modern-ci-cd-pipeline-5faa01891a5b
+# It was modified by Uri to better match the recommended way of using 'unittest' framework as follows:
+#   1. Added a class wrapping all the method
+#   2. The class is derived from 'unittest.TestCase' class
+#   3. Asserts were changed
 
+import unittest
 from buzz import generator
 
-def test_sample_single_word():
-    l = ('foo', 'bar', 'foobar')
-    word = generator.sample(l)
-    assert word in l
+class PrimesTestCase(unittest.TestCase):
+    """Tests for `generator.py`"""
 
-def test_sample_multiple_words():
-    l = ('foo', 'bar', 'foobar')
-    words = generator.sample(l, 2)
-    assert len(words) == 2
-    assert words[0] in l
-    assert words[1] in l
-    assert words[0] is not words[1]
+    def test_sample_single_word(self):
+        l = ('foo', 'bar', 'foobar')
+        word = generator.sample(l)
+        self.assertTrue(word in l)
 
-def test_generate_buzz_of_at_least_five_words():
-    phrase = generator.generate_buzz()
-    assert len(phrase.split()) >= 5
+    def test_sample_multiple_words(self):
+        l = ('foo', 'bar', 'foobar')
+        words = generator.sample(l, 2)
+        self.assertTrue(len(words) == 2)
+        self.assertTrue(words[0] in l)
+        self.assertTrue(words[1] in l)
+        self.assertFalse(words[0] is words[1])
+
+    def test_generate_buzz_of_at_least_five_words(self):
+        phrase = generator.generate_buzz()
+        self.assertTrue(len(phrase.split()) >= 5)
+
+if __name__ == '__main__':
+    unittest.main()
